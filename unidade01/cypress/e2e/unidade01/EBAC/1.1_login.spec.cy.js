32d81a91ba37881d2565eee0c72e466414410449
Gilvan QA <gillvanjs@gmail.com>
2024-03-01 14:48:56 -0300

    teste

--- unidade01/cypress/e2e/unidade01/EBAC/1.1_login.spec.cy.js
+++ unidade01/cypress/e2e/unidade01/EBAC/1.1_login.spec.cy.js
@@ -28,5 +28,3 @@ context('Funcionalidade Login', () => {
         cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
     })
 })
-
-teste
